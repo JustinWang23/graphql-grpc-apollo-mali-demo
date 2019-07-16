715efbd0d521865b7487edf76f2c1bc0fe31cfd1
Justin Wang <junting.wang@thoughtworks.com>
2019-07-16 12:14:10 +0800

    chore: move graphql schema defination to a single file

--- node-graphql/index.ts
+++ node-graphql/index.ts
@@ -1,16 +1,7 @@
-import { ApolloServer, makeExecutableSchema } from 'apollo-server-express'
+import { ApolloServer } from 'apollo-server-express'
 import express from 'express'
-import * as path from 'path'
-import * as fs from 'fs'
 
-import { resolvers } from './resolvers'
-
-const schemaPublic = fs.readFileSync(path.resolve(__dirname, './schema/schema.graphql')).toString('utf8')
-
-const schema = makeExecutableSchema({
-  resolvers,
-  typeDefs: [schemaPublic]
-})
+import { schema } from './graphql'
 
 const server = new ApolloServer({ schema })
 
