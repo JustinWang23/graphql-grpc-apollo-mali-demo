2c96e2c0e2efeff85328ae07a6b48085c64d76d6
Justin Wang <junting.wang@thoughtworks.com>
2019-07-16 15:53:04 +0800

    feat: add argument for graphql query

--- node-graphql/graphql.ts
+++ node-graphql/graphql.ts
@@ -2,11 +2,11 @@ import { makeExecutableSchema } from 'apollo-server-express'
 import * as path from 'path'
 import * as fs from 'fs'
 
-import { resolvers } from './resolvers'
+import resolvers from './resolvers'
 
 const schemaPublic = fs.readFileSync(path.resolve(__dirname, './schema/schema.graphql')).toString('utf8')
 
 export const schema = makeExecutableSchema({
   resolvers,
-  typeDefs: [schemaPublic]
+  typeDefs: [schemaPublic],
 })
--- node-graphql/resolvers/index.ts
+++ node-graphql/resolvers/index.ts
@@ -1,5 +1,35 @@
-export const resolvers = {
+const Books = [{
+  title: 'Talent',
+  author: {
+    name: 'Justin',
+    age: 26,
+  },
+  price: 37.8,
+  categories: [
+    'Education',
+    'Popular',
+  ]
+}, {
+  title: 'GraphQL',
+  author: {
+    name: 'Somebody',
+    age: 43,
+  },
+  price: 102.5,
+  categories: [
+    'Skills',
+  ]
+}]
+
+const resolvers = {
   Query: {
-    hello: () => 'world'
+    listBooks: (root: any, args: any) => {
+      if(args.title) {
+        return Books.filter((book) => book.title === args.title)
+      }
+      return Books
+    }
   }
 }
+
+export default resolvers
