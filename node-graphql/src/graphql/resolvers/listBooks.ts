d02000d1867f5d92976818b12faeaef193305436
Justin Wang <junting.wang@thoughtworks.com>
2019-07-22 17:27:45 +0800

    chore: clean unused code

--- node-graphql/src/graphql/resolvers/listBooks.ts
+++ node-graphql/src/graphql/resolvers/listBooks.ts
@@ -1,39 +1,11 @@
 import { GrpcClient } from '../../grpc'
 
-const Books = [{
-  id: 1,
-  title: 'Talent',
-  price: 37.8,
-  categories: [
-    'Education',
-    'Popular',
-  ],
-  authorId: 1,
-}, {
-  id: 2,
-  title: 'GraphQL',
-  price: 102.5,
-  categories: [
-    'Skills',
-  ],
-  authorId: 2,
-}, {
-  id: 3,
-  title: 'Talent is not enough',
-  price: 146.2,
-  categories: [
-    'Skills',
-    'Popular',
-  ],
-  authorId: 1,
-}]
-
 interface Params {
   title: string
 }
 
 interface Book {
-  id: number
+  id: number 
   title: string
   price: number
   categories: string[]
