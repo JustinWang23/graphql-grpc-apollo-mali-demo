d6d57c977b7dda5e97cc176080e41b999776fe83
Justin Wang <junting.wang@thoughtworks.com>
2019-07-23 10:53:53 +0800

    chore: replace mock with server data for authors

--- node-graphql/src/graphql/resolvers/author.ts
+++ node-graphql/src/graphql/resolvers/author.ts
@@ -1,12 +1,4 @@
-const Authors = [{
-  id: 1,
-  name: 'Justin',
-  age: 26,
-}, {
-  id: 2,
-  name: 'Somebody',
-  age: 43,
-}]
+import { GrpcClient } from '../../grpc'
 
 interface Author {
   id: number
@@ -14,6 +6,8 @@ interface Author {
   age: number
 }
 
-export default (root: any, params: any) => {
-  return Authors.find((author: Author) => author.id === root.authorId)
+export default async (root: any, params: any) => {
+  const client: any = new GrpcClient('0.0.0.0:50051', 'book', 'books.proto').getService('BookService')
+  const result = await client.getAuthors().sendMessage()
+  return result.authors.find((author: Author) => author.id === root.authorId)
 }
