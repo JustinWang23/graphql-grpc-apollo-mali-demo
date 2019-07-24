41e50a3c734f44aede93d8f6566945dea7169653
Justin Wang <junting.wang@thoughtworks.com>
2019-07-24 11:25:05 +0800

    feat: add error log to graphQL server

--- graphql-server/src/server.ts
+++ graphql-server/src/server.ts
@@ -2,7 +2,13 @@ import { ApolloServer } from 'apollo-server'
 
 import { schema } from './graphql'
 
-const app = new ApolloServer({ schema })
+const app = new ApolloServer({
+  schema,
+  formatError: error => {
+    console.log(error)
+    return error
+  },
+})
 
 app.listen({ port: 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`)
